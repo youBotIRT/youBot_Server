54cda19191d9225fa81aa62fa326b6eaaf2d8897
Fabian Buse <fabian.buse@rwth-aachen.de>
2014-07-17 12:32:54 +0200

    Created Setup Script

--- youBot_Scripts/setup.sh
+++ youBot_Scripts/setup.sh
@@ -1,3 +1,74 @@
 #!/bin/sh
 
-sudo setcap cap_net_raw+ep ../youBot_Server/bin/youBot_Server
+
+echo 'Building all Appilcations' 
+
+cd ..
+
+cd youBot_Client
+rm build/ -r
+rm lib/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+cd youBot_Server
+rm build/ -r
+rm bin/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+cd youBot_JoyPad
+rm build/ -r
+rm bin/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+cd youBot_Info
+rm build/ -r
+rm bin/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+cd youBot_Monitor
+rm build/ -r
+rm bin/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+cd youBot_UDP
+rm build/ -r
+rm bin/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+cd youBot_PID_Tuner
+rm build/ -r
+rm bin/ -r
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+echo 'Making Ethercat and the LCD Display accesable without Sudo'
+sudo setcap cap_net_raw+ep /youBot_Server/bin/youBot_Server
+sudo chmod 666 /dev/ttyACM0
+
